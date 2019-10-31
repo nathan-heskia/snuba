ccd76d06cba0e0534d7d33d6e299312408129e91
Ted Kaemming <ted@kaemming.com>
2019-10-31 15:47:37 -0700

    add kafka producer backend

---
+++ snuba/utils/streams/producers/backends/kafka.py
@@ -0,0 +1,27 @@
+from typing import Any, Mapping, NamedTuple, Optional
+
+from confluent_kafka import Producer
+
+from snuba.utils.streams.producers.backends.abstract import ProducerBackend
+
+
+class TopicPartition(NamedTuple):
+    topic: str
+    partition: Optional[int]
+
+
+class KafkaProducerBackend(ProducerBackend[TopicPartition, bytes]):
+
+    def __init__(self, configuration: Mapping[str, Any]) -> None:
+        self.__producer = Producer(configuration)
+
+    def produce(self, stream: TopicPartition, value: bytes) -> None:
+        kwargs = {}
+        if stream.partition is not None:
+            kwargs['partition'] = stream.partition
+        self.__producer.produce(stream.topic, value, **kwargs)
+        return None
+
+    def flush(self) -> int:
+        size: int = self.__producer.flush()
+        return size
